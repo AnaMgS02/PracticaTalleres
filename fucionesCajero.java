e5c3fb1b84d83432cacde1ea0f2b401756981650
AnaMgS02 <anamariagomez102@gmail.com>
2023-08-23 09:29:34 -0500

    h

--- fucionesCajero.java
+++ fucionesCajero.java
@@ -3,19 +3,58 @@ public class fucionesCajero {
     public int[] saldo = { 0, 0, 0, 0, 0 };
     String[] transacion = { "" };
 
-    public int consultaSaldo(int cuenta){
-        int i=0;
-        for(i=0;i<=cuentas.length;i++){
-            if(cuenta==cuentas[i]){
+    public int consultaSaldo(int cuenta) {
+        int i = 0;
+        for (i = 0; i <= cuentas.length; i++) {
+            if (cuenta == cuentas[i]) {
                 break;
             }
         }
-        int j=0;
-        for(j=0;j<=saldo.length;j++){
-            if(i==j){
+        int j = 0;
+        for (j = 0; j <= saldo.length; j++) {
+            if (i == j) {
                 break;
             }
         }
         return saldo[j];
+    }
+
+    public boolean retiro(int cuenta, int valor) {
+        int i = 0;
+        for (i = 0; i <= cuentas.length; i++) {
+            if (cuenta == cuentas[i]) {
+                break;
+            }
+        }
+        int j = 0;
+        for (j = 0; j <= saldo.length; j++) {
+            if (i == j) {
+                break;
+            }
+        }
+        if (valor > saldo[j]) {
+            System.out.println("Saldo insuficiente");
+            return false;
+        } else {
+            System.out.println("Retiro realizado");
+            return true;
+        }
+    }
+
+    public void consignacion(int cuenta, int valor) {
+        int i = 0;
+        for (i = 0; i <= cuentas.length; i++) {
+            if (cuenta == cuentas[i]) {
+                break;
+            }
+        }
+        int j = 0;
+        for (j = 0; j <= saldo.length; j++) {
+            if (i == j) {
+                saldo[j] = saldo[j] + valor;
+                System.out.println("Consignación realizada");
+                break;
+            }
+        }
     }
 }
---
+++ main.java
@@ -0,0 +1,5 @@
+public class main {
+    public static void main(String args[]) {
+        // Usar un while o do while para realizar el historial de transaciones
+    }
+}
