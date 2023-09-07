9f7c229b0bd406e1991abfd96837cc1d700adb4f
AnaMgS02 <anamariagomez102@gmail.com>
2023-09-07 07:53:59 -0500

    y

--- EjercicioMatrices1/funcionMatriz.java
+++ EjercicioMatrices1/funcionMatriz.java
@@ -5,68 +5,111 @@ import java.util.Scanner;
 import Taller2.vector.mainVector;
 
 public class funcionMatriz {
-    Scanner sc=new Scanner
+    // matriz.lenght # de filas, matriz.lenght[0] # de columnas
+    Scanner sc = new Scanner(System.in);
     int[][] matriz1 = { { 3, 6, 9, 4 }, { 2, 5, 7, 1 } };
     int[][] matriz2 = { { 3, 1, 9, 6 }, { 6, 2, 8, 7 } };
-    int[][] matriz=new int[4][4];
+    int[][] matriz = new int[4][4];
     int[][] matrizR = new int[2][4];
 
-    public void sumaMatrices(int[][] matriz1, int[][]matriz2) {
+    public void sumaMatrices() {
         for (int i = 0; i < 2; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.print(" " + (matriz1[i][j] + matriz2[i][j]));
+                matrizR[i][j] = matriz1[i][j] + matriz2[i][j];
             }
             System.out.println();
         }
     }
-    public void mostrarMatriz() {
+
+    public void mostrarMatrizR() {
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" " + matrizR[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+    public void addNumero() {
+        int numero = 0;
         for (int i = 0; i < 4; i++) {
             for (int j = 0; j < 4; j++) {
-                System.out.print(" "+matriz[i][j]);
+                System.out.print("Ingresa nuevo elemento: ");
+                numero = sc.nextInt();
+                matriz[i][j] = numero;
             }
             System.out.println();
         }
     }
-    public void addNumero(int num){
+
+    public int sumarFila(int fila) {
+        int suma = 0;
         for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                matriz[i][j]=num;
-            }
-            System.out.println();
-        }
-    }
-    public int sumarFila(int fila){
-        int suma=0;
-        for (int i = 0; i < 4; i++) {
-            suma+=matriz[fila][i];
+            suma += matriz[fila - 1][i];
         }
         return suma;
     }
-    public int sumarColumna(int columna){
-        int suma=0;
+
+    public int sumarColumna(int columna) {
+        int suma = 0;
         for (int i = 0; i < 4; i++) {
-            suma+=matriz[i][columna];
+            suma += matriz[i][columna - 1];
         }
         return suma;
     }
-    
-    public int sumarDiagonalP(){
-        int suma=0;
+
+    public int sumarDiagonalP() {
+        int suma = 0;
         for (int i = 0; i < 4; i++) {
-            for(int j=0;j<4;j++){
-                if(i==j){
-                    suma+=matriz[i][j];
+            for (int j = 0; j < 4; j++) {
+                if (i == j) {
+                    suma += matriz[i][j];
                 }
             }
         }
         return suma;
     }
-    public int sumarDiagonalS(){
-        int suma=0;
-        for (int i = 1; i < 4; i++) {
-            suma+=matriz[i][matriz.length-i];
+
+    public int sumarDiagonalS() {
+        int suma = 0;
+        int j=3;
+        for (int i = 0; i < 4; i++) {
+            suma =suma +matriz[i][j];
+            j--;
         }
         return suma;
     }
-    public 
+
+    public double promedioMatriz() {
+        int suma = 0, cont = 0;
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                suma += matriz[i][j];
+                cont++;
+            }
+        }
+        return suma / cont;
+    }
+
+    public void mostrarMatriz() {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(" " + matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    int[][] matrizA = { { 1, 2 }, { 3, 4 } };
+    int[][] matrizB = { { 3, -2 }, { 1, 5 } };
+    int[][] matrizC = { { 4, -1 }, { -2, 3 } };
+    int[][] matrizD = { { -2, 5 }, { 6, -3 } };
+    int[][] matrizE = { { 1, 2, 0 }, { 3, 2, -1 }, { 5, 1, -2 } };
+    int[][] matrizF = { { 3, 4, 0 }, { 1, 0, -2 }, { -1, 2, 1 } };
+    public void producto(){
+
+        
+    }
+    int[][] matrizG = { { 2, 4 }, { -3, 5 } };
+    int[][] matrizH = { { -1, -2 }, { 3, -3 } };
 }
--- EjercicioMatrices1/main.java
+++ EjercicioMatrices1/main.java
@@ -1,6 +1,71 @@
 package EjercicioMatrices1;
-public class main{
+
+import java.util.Scanner;
+
+public class main {
     public static void main(String[] args) {
-        
+        funcionMatriz fc = new funcionMatriz();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ejercicio que desea ver: ");
+        System.out.println("A\nB\nC\n Escriba la letra en mayuscula");
+        char ejercicio = sc.next().charAt(0);
+        switch (ejercicio) {
+            case 'A':
+                fc.sumaMatrices();
+                fc.mostrarMatrizR();
+                break;
+            case 'B':
+                int volverB = 1;
+                while (volverB == 1) {
+                    System.out.println("Que operación desea hacer");
+                    System.out.println("1. Llenar TODA la matriz 4x4 con numeros ");
+                    System.out.println("2. Suma una fila completa");
+                    System.out.println("3. Suma una columna");
+                    System.out.println("4. Sumar la diagonal principal");
+                    System.out.println("5. Sumar la diagonal inversa");
+                    System.out.println("6. Imprimir el promedio de todos los valores de la matriz");
+                    char hacer = sc.next().charAt(0);
+                    int numero = 0;
+                    switch (hacer) {
+                        case '1':
+                            fc.addNumero();
+                            fc.mostrarMatriz();
+                            break;
+                        case '2':
+                            fc.mostrarMatriz();
+                            System.out.println("Ingrese el número de fila que desea sumar");
+                            numero = sc.nextInt();
+                            System.out.println(fc.sumarFila(numero));
+                            break;
+                        case '3':
+                            fc.mostrarMatriz();
+                            System.out.println("Ingrese el número de columna que desea sumar");
+                            numero = sc.nextInt();
+                            System.out.println(fc.sumarColumna(numero));
+                            break;
+                        case '4':
+                            fc.mostrarMatriz();
+                            System.out.print("Diagonal Principal: ");
+                            System.out.println(fc.sumarDiagonalP());
+                            break;
+                        case '5':
+                            fc.mostrarMatriz();
+                            System.out.print("Diagonal Secundaria: ");
+                            System.out.println(fc.sumarDiagonalS());
+                            break;
+                        case '6':
+                            fc.mostrarMatriz();
+                            System.out.print("Promedio de valor de elementos: ");
+                            System.out.println(fc.promedioMatriz());
+                            break;
+                        default:
+                            break;
+                    }
+                    System.out.println("Desea volver al menu?");
+                    volverB=sc.nextInt();
+                }
+            default:
+                break;
+        }
     }
 }
